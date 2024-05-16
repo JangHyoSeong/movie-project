d9217e410b9a3c24793903c3a4df5b64dc5ff4b9
kdhn97 <ehdgus3726@naver.com>
2024-05-16 17:52:20 +0900

    메인화면 구성

--- final-pjt/final-pjt-front/src/App.vue
+++ final-pjt/final-pjt-front/src/App.vue
@@ -1,85 +1,66 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+  <header class="navbar">
+    <div class="navbar-background"></div>
+    <nav class="navbar-content">
+        <img src="../public/Logo_img.png" alt="Logo" class="logo">
+        <RouterLink :to="{ name: 'home' }" class="nav">다각화</RouterLink>
+      <div class="nav_sign">
+        <RouterLink :to="{ name: 'login' }" class="nav login">로그인</RouterLink>
+        <RouterLink :to="{ name: 'signup' }" class="nav signup">회원가입</RouterLink>
+      </div>
+    </nav>
   </header>
 
   <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style>
+body {
+  margin: 0;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
+/* 네비게이션 바 */
+.navbar {
+  position: relative;
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  height: 50px;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.navbar-background {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #000000;
 }
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.navbar-content {
+  position: relative;
+  display: flex;
+  align-items: center;
+  height: 100%;
 }
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.nav {
+  text-decoration: none;
+  color: white;
 }
-
-nav a:first-of-type {
-  border: 0;
+.nav_sign {
+  position: absolute;
+  right: 0%;
+  width: 12%;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.login, .signup {
+  margin: 3%;
+  padding: 3%;
+  text-decoration: none;
+  color: white;
+  border: 1px solid white;
+}
+.login:hover, .signup:hover {
+  background-color: #166AE8;
+  border: none;
+}
+.logo {
+  width: 50px;
+  height: 50px;
 }
 </style>
--- final-pjt/final-pjt-front/src/views/HomeView.vue
+++ final-pjt/final-pjt-front/src/views/HomeView.vue
@@ -1,9 +1,129 @@
+<template>
+  <img src="../../public/background_img.png" alt="background_img" class="background_img">
+  
+  <div class="background">
+    <div class="main_txt">
+      <h5>어떤 영화를 찾으시나요?</h5>
+      <h1>당신을 위한 영화 추천 사이트</h1>
+      <h5>당신의 선택을 통하여 다양한 각도에서 영화를 추천해드립니다</h5>
+      <p class="recommend">추천받기</p>
+    </div>
+
+    <div class="movie_info_cnt">
+      <div class="movie_cnt">
+        <h2>999</h2>
+        <h2>100</h2>
+        <h2>100</h2>
+        <h2>100</h2>
+        <h2>100</h2>
+      </div>
+      <div class="movie_info">
+        <h2>영화 수</h2>
+        <h2>장르 수</h2>
+        <h2>감독 수</h2>
+        <h2>배우 수</h2>
+        <h2>국가 수</h2>
+      </div>
+      <h3 class="scroll">Scroll ▽</h3>
+    </div>
+
+    <div class="new_movie">
+      <h1>[ 현재 개봉작 ]</h1>
+        <div class="poster">
+          <p class="post"></p>
+          <p class="post"></p>
+          <p class="post"></p>
+          <p class="post"></p>
+          <p class="post"></p>
+          <p class="post"></p>
+        </div>
+      <h1>[ 개봉 예정작 ]</h1>
+      <div class="poster">
+          <p class="post"></p>
+          <p class="post"></p>
+          <p class="post"></p>
+          <p class="post"></p>
+          <p class="post"></p>
+          <p class="post"></p>
+        </div>
+    </div>
+  </div>
+
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+/* 배경 사이즈 */
+.background {
+  height: 2000px;
+}
+
+.background_img {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+}
+
+/* 메인화면 텍스트 */
+.main_txt {
+  width: 100%;
+  height: 30%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 200%;
+  color: white;
+}
+.recommend {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 10%;
+  height: 10%;
+  border: 1px solid white;
+  color: white;
+}
+.recommend:hover {
+  background-color: #166AE8;
+  border-style: none;
+}
+
+/* 영화 정보 개수 */
+.movie_info_cnt {
+  width: 100%;
+  height: 13.5%;
+}
+.movie_cnt, .movie_info {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  color: white;
+}
+.scroll {
+  display: flex;
+  justify-content: center;
+  color: white;
+}
+
+/* 상영 영화 목록 */
+.new_movie {
+  width: 100%;
+  height: 55%;
+  background-color: rgb(255, 132, 132);
+  text-align: center;
+}
+.poster {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+.post {
+  width: 200px;
+  height: 250px;
+  background-color: gray;
+}
+</style>
