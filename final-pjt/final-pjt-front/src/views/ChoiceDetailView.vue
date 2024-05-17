b92e0fc08032c3f52fbff70e186f5d30ec3f4333
kdhn97 <ehdgus3726@naver.com>
2024-05-17 15:05:30 +0900

    화면구성

--- final-pjt/final-pjt-front/src/views/ChoiceDetailView.vue
+++ final-pjt/final-pjt-front/src/views/ChoiceDetailView.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="order">시간순</div>
-  <div class="order">평점순</div>
+  <div class="order-list">
+    <div class="order-time">시간순</div>
+    <div class="order-range">평점순</div>
+  </div>
 
   <div class="poster">
+    <p class="post"></p>
     <p class="post"></p>
     <p class="post"></p>
     <p class="post"></p>
@@ -30,70 +33,75 @@
       <p>방영일 : 24.05.10</p>
       <p>평점 : 5.0</p>
     </div>
-  </div>
-
-  <div class="poster">
-    <p class="post"></p>
-    <p class="post"></p>
-    <p class="post"></p>
-    <p class="post"></p>
-  </div>
-
-  <div class="movie-content-list">
-    <div class="movie-content">
-      <p>제목 : 쇼생크 탈출</p>
-      <p>방영일 : 24.05.10</p>
-      <p>평점 : 5.0</p>
-    </div>
-    <div class="movie-content">
-      <p>제목 : 쇼생크 탈출</p>
-      <p>방영일 : 24.05.10</p>
-      <p>평점 : 5.0</p>
-    </div>
-    <div class="movie-content">
-      <p>제목 : 쇼생크 탈출</p>
-      <p>방영일 : 24.05.10</p>
-      <p>평점 : 5.0</p>
-    </div>
     <div class="movie-content">
       <p>제목 : 쇼생크 탈출</p>
       <p>방영일 : 24.05.10</p>
       <p>평점 : 5.0</p>
     </div>
   </div>
+  <p class="go-to-select" @click="goToSelect">돌아가기</p>
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router'
 
+const router = useRouter()
+
+const goToSelect = function () {
+  router.push({ name: 'choice' })
+}
 </script>
 
 <style scoped>
-.order {
-  display: inline-block;
-  border: 1px solid black;
-  margin: 3% 0% 1% 7%;
-  padding: 0.5%;
-}
-
-.poster {
+.order-list {
   display: flex;
-  justify-content: space-evenly;
+  padding: 2% 0% 0.5% 3.2%;
 }
 
-.post {
-  width: 250px;
-  height: 350px;
-  background-color: black;
+.order-time,
+.order-range {
+  width: 50px;
+  border: 1px solid white;
+  margin: 0.5%;
+  padding: 0.5%;
+  color: white;
 }
 
+.order-time:hover,
+.order-range:hover,
+.go-to-select:hover {
+  background-color: #166AE8;
+  border-style: none;
+  color: white;
+}
+
+.poster,
 .movie-content-list {
   display: flex;
   justify-content: space-evenly;
 }
 
+.post {
+  margin: 0%;
+  padding: 0%;
+  width: 300px;
+  height: 450px;
+  background-color: rgb(200, 200, 200);
+}
+
 .movie-content {
+  width: 300px;
   text-align: center;
-  width: 250px; 
-  border: 1px solid black;
+  color: white;
+}
+
+.go-to-select {
+  position: relative;
+  left: 47.8%;
+  width: 5%;
+  color: white;
+  padding: 0.5% 0%;
+  text-align: center;
+  border: 1px solid white;
 }
 </style>
