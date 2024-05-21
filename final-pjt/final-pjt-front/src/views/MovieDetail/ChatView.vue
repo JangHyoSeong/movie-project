994eed7fa4e29b681982619f41f2ef2fbe49faff
JHS <wkdgytmd200@naver.com>
2024-05-21 14:51:47 +0900

    채팅 기능 개선

--- final-pjt/final-pjt-front/src/views/MovieDetail/ChatView.vue
+++ final-pjt/final-pjt-front/src/views/MovieDetail/ChatView.vue
@@ -1,15 +1,22 @@
 <template>
+  <!-- 채팅창 전체를 감싸는 div -->
   <div class="chat-container">
+
+    <!-- 지금까지의 채팅이 들어가는 영역 -->
     <div class="chat-list">
-      <div v-for="chat in chatList" class="chat">
+      <!-- 채팅 -->
+      <div v-for="chat in chatList" class="chat" :class="{ 'my-chat': currentUser.id === chat.user.id }">
         <p>채팅 유저 : {{ chat.user.nickname }}</p>
         <p>채팅 : {{ chat.content }}</p>
       </div>
     </div>
+
+    <!-- 채팅 입력구간 -->
     <form @submit.prevent="chatSubmit">
       <input class="chat-input" type="text" placeholder="NewComment" v-model="chatMessage">
       <input type="submit" value="submit">
     </form>
+
   </div>
 </template>
 
@@ -69,6 +76,29 @@ const chatSubmit = function () {
     .catch(err => console.log(err))
 }
 
+// 1초마다 채팅을 새로불러옴
+const repeat = setInterval(loadMessage, 1000)
+
+// 현재 로그인된 유저 정보 가져오기
+const currentUser = ref(null)
+onMounted(() => {
+  axios({
+    method: 'get',
+    url: 'http://127.0.0.1:8000/api/v1/current_user/',
+    headers: {
+      Authorization: `Token ${store.token}`
+    }
+  })
+    .then((res) => {
+      currentUser.value = res.data
+    })
+    .catch(err => console.log(err))
+})
+
+// 현재 유저가 리뷰 작성자인지 확인
+const isCurrentUser = function (user) {
+  return currentUser.value && user.id === currentUser.value.id
+}
 </script>
 
 <style scoped>
@@ -87,5 +117,11 @@ const chatSubmit = function () {
   height: 30px;
   width: 100%;
   bottom: 0;
+}
+
+/* 내 채팅 */
+.my-chat{
+  text-align: right;
+  color: red;
 }
 </style>
